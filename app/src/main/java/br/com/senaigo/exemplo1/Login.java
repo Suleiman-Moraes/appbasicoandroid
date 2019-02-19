793f9dcb299ce3fda6ba63f7d9c103dc2b660dc1
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-02-19 17:05:04 -0300

    Atualização

---
+++ app/src/main/java/br/com/senaigo/exemplo1/Login.java
@@ -0,0 +1,34 @@
+package br.com.senaigo.exemplo1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    private EditText usuario;
+    private EditText senha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.usuario = findViewById(R.id.login);
+        this.senha = findViewById(R.id.senha);
+    }
+
+    public void verificarLogin(View view) {
+        String pegaUsuario = usuario.getText().toString();
+        String pegaSenha = senha.getText().toString();
+
+        if (pegaSenha.trim().equals("123") && pegaUsuario.trim().equals("susu")) {
+            Intent intent = new Intent(this, Principal.class);
+            startActivity(intent);
+        } else {
+
+        }
+    }
+}

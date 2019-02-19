010f803694684b554129820734d86f5bcf349bca
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-02-19 15:41:12 -0300

    Atualização

--- app/src/main/java/br/com/senaigo/exemplo1/MainActivity.java
+++ app/src/main/java/br/com/senaigo/exemplo1/MainActivity.java
@@ -1,7 +1,7 @@
 package br.com.senaigo.exemplo1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.editText = findViewById(R.id.editText);
-        this.mensagemText = (TextView) findViewById(R.id.mensagemText);
+        this.mensagemText = findViewById(R.id.mensagemText);
     }
 
     public void exibirMensagem(View view) {
